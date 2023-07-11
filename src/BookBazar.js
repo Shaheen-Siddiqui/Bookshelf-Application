eb73e1874c220823ca6f11617199d582df2d4c90
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-11 14:07:33 +0530

    feat: bookBazar database

---
+++ src/BookBazar.js
@@ -0,0 +1,81 @@
+export const BookBazar = [
+  {
+    id: "1",
+    name: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    category: "read",
+    imageUrl:
+      "https://3.bp.blogspot.com/-BTxRewZbt4g/VfEAxp6IeuI/AAAAAAAAXWw/LILxuSsCwjg/s1600/advancedpotions.jpg",
+  },
+  {
+    id: "2",
+    name: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+    category: "currently reading",
+    imageUrl:
+      "https://kidsactivitiesblog.com/wp-content/uploads/2020/09/how-to-make-your-own-harry-potter-spell-book-using-free-printable-coloring-pages.jpg",
+  },
+  {
+    id: "3",
+    name: "Harry Potter and the Prisoner of Azkaban",
+    author: "J.K. Rowling",
+    category: "want to read",
+    imageUrl:
+      "https://s-media-cache-ak0.pinimg.com/originals/c3/19/ee/c319eef95110daab3cb61946d15ed3bf.jpg",
+  },
+  {
+    id: "4",
+    name: "Harry Potter and the Goblet of Fire",
+    author: "J.K. Rowling",
+    category: "none",
+    imageUrl:
+      "https://www.notquitesusie.com/wp-content/uploads/2017/01/Harry-Potter-Bookmark-Sorcerers-Stone.jpg",
+  },
+  {
+    id: "5",
+    name: "Harry Potter and the Order of the Phoenix",
+    author: "J.K. Rowling",
+    category: "read",
+    imageUrl:
+      "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_360,c_scale/jackets/9780747560777.jpg",
+  },
+  {
+    id: "6",
+    name: "Harry Potter and the Half-Blood Prince",
+    author: "J.K. Rowling",
+    category: "currently reading",
+    imageUrl: "https://i.ytimg.com/vi/ZJ1ej9O3QZo/maxresdefault.jpg",
+  },
+  {
+    id: "7",
+    name: "Harry Potter and the Deathly Hallows",
+    author: "J.K. Rowling",
+    category: "want to read",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_JrSAW5-pXjX5vawrY4TKrxKX6lBHujhoP6HQKbAvHfXVB-dl",
+  },
+  {
+    id: "8",
+    name: "Fantastic Beasts and Where to Find Them",
+    author: "J.K. Rowling",
+    category: "none",
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS8a3eALnnvAE-fWV16jzJBJX2fkjvsP_vtPmy_tOCce35VHuqF",
+  },
+  {
+    id: "9",
+    name: "Quidditch Through the Ages",
+    author: "Kennilworthy Whisp (J.K. Rowling)",
+    category: "read",
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQhf13ky1bmK7mn73AHzcLxXruz7EX9ZDZcE4RVn95gfUtL_gZV",
+  },
+  {
+    id: "10",
+    name: "The Tales of Beedle the Bard",
+    author: "J.K. Rowling",
+    category: "currently reading",
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-Xxi9wdImhMsEqYUDRUVR0rwNhapTEY7dUAnMF3HJSl8FQxR2",
+  },
+];
