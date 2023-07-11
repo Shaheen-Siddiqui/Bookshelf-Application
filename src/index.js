22646cabc6e021345a276909f26f30f440a4a5ee
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-11 14:08:39 +0530

    refactor: wraped children into book context provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { BookShelfContextProvider } from "./hooks/context/BookShelfContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <BookShelfContextProvider>
+        <App />
+      </BookShelfContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
