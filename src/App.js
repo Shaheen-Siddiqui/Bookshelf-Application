005cb34ef96410509f2e44a28a7078b086c507a5
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-11 14:07:02 +0530

    feat: routing implimenttion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { BookListing } from "./pages/BookListing";
+import { SearchBooks } from "./pages/SearchBooks";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<BookListing />} />
+        <Route path="/search" element={<SearchBooks />} />
+        <Route
+          path="/question"
+          element={
+            <Link to="https://www.awesomescreenshot.com/image/41402636?key=821a17f0d833d571786f0b388e3896eb">
+              Here
+            </Link>
+          }
+        />
+      </Routes>
     </div>
   );
 }
