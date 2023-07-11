9f716a4908ca4ab023d088f4cbce6c1f35d3687b
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-11 14:09:16 +0530

    feat: a brand new ook controller component

---
+++ src/components/BookCantrolar.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { BookShelfContext } from "../hooks/context/BookShelfContext";
+
+export const BookCantrolar = ({
+  bookCategory,
+  bookId,
+  setIsControllerOpen,
+}) => {
+  const { BookCategoryArray, dispatchBookshelfState } =
+    useContext(BookShelfContext);
+
+  return (
+    <div className="list-group">
+      {[...BookCategoryArray,"none"].map((categories, index) => {
+        return (
+          <li
+            key={index}
+            onClick={() => {
+              dispatchBookshelfState({
+                type: "SELECTED_CATEGORY",
+                payload: { categories, bookId },
+              });
+              setIsControllerOpen(false);
+            }}
+            className="list-group-item"
+            id={bookCategory === categories ? "list-color" : "point"}
+          >
+            {categories}
+          </li>
+        );
+      })}
+    </div>
+  );
+};
